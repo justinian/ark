e52f86ce15561534e7eed44e4eb2fc87b4fd9b4e
Justin C. Miller <justin@devjustinian.com>
2021-08-21 19:26:32 -0700

    Display a map and location on select

--- cmd/server/static/main.js
+++ cmd/server/static/main.js
@@ -38,7 +38,47 @@ var showStats = function () {
 
 var fixedFloat = function (data, type, row, meta) {
     return data.toFixed(0);
-}
+};
+
+var maps = {
+    "TheIsland": {"shiftx": 0.5, "shifty": 0.5, "mulx": 800000, "muly": 800000},
+    "ScorchedEarth": {"shiftx": 0.5, "shifty": 0.5, "mulx": 800000, "muly": 800000},
+    "Aberration": {"shiftx": 0.5, "shifty": 0.5, "mulx": 800000, "muly": 800000},
+    "Extinction": {"shiftx": 0.5, "shifty": 0.5, "mulx": 800000, "muly": 800000},
+    "Ragnarok": {"shiftx": 0.5, "shifty": 0.5, "mulx": 1310000, "muly": 1310000},
+    "Valguero": {"shiftx": 0.5, "shifty": 0.5, "mulx": 816000, "muly": 816000},
+    "CrystalIsles": {"shiftx": 0.4875, "shifty": 0.5, "mulx": 1600000, "muly": 1700000},
+    "Genesis1": {"shiftx": 0.5, "shifty": 0.5, "mulx": 1050000, "muly": 1050000},
+    "Genesis2": {"shiftx": 0.49655, "shifty": 0.49655, "mulx": 1450000, "muly": 1450000},
+};
+
+var drawMap = function (canvas, world, x, y) {
+    var mapInfo = maps[world];
+    if (mapInfo === undefined) {
+        return;
+    }
+
+    var mapImage = new Image();
+    mapImage.onload = function () {
+        canvas.height = mapImage.height;
+        canvas.width = mapImage.width;
+
+        ctx = canvas.getContext("2d");
+        ctx.drawImage(mapImage, 0, 0);
+
+        mx = (x / mapInfo["mulx"] + mapInfo["shiftx"]) * canvas.width;
+        my = (y / mapInfo["muly"] + mapInfo["shifty"]) * canvas.height;
+        pointSize = canvas.width / 125;
+
+        ctx.fillStyle = "#ff2020";
+        ctx.beginPath();
+        ctx.arc(mx, my, pointSize, 0, 2 * Math.PI, true);
+        ctx.fill();
+
+        canvas.style.width = "100%";
+    };
+    mapImage.src = "images/maps/" + world + ".webp";
+};
 
 var columns = [
     {"data": "name", "title": "Name"},
@@ -98,7 +138,7 @@ var columns = [
     {"data": "weight_total", "title":"W", "searchBuilderTitle": "Total Weight"},
     {"data": "melee_total", "title":"M", "searchBuilderTitle": "Total Melee"},
     {"data": "speed_total", "title":"Sp", "searchBuilderTitle": "Total Speed"}
-]
+];
 
 var tableOptions = {
     "ajax": {"url":"api/dinos", "dataSrc":""},
@@ -126,4 +166,4 @@ var tableOptions = {
                     34, 35, 36, 37, 38, 39, 40, 41,
                     42, 43, 44, 45, 46, 47, 48, 49]
     }
-}
+};
